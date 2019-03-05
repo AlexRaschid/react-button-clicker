a8764e3450f57ee81b5506e11bea98701e096dce
AlexRaschid <alexraschid1337@gmail.com>
2019-03-05 18:40:23 -0500

    Init/Simple button clicker in react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//ButtonClicks Component made
+class ButtonClicks extends React.Component {
+    
+    //ES6 Constructor sets the state of clicks to 0
+    constructor(props){
+        super(props);
+        this.state = {
+            clicks: 0
+
+        }
+
+        //Setting the context of the .this 
+        this.onButtonClick = this.onButtonClick.bind(this);
+    }
+
+    //Changes the click state
+    onButtonClick(){
+        this.setState({
+            clicks: this.state.clicks + 1
+        });
+    }
+
+
+    
+    //The actual html of the button
+    render(){
+        return (
+            <div>
+                <h1>This is my button clicker made with react. I hope you enjoy</h1>
+                     <h3>Clicks: {this.state.clicks}</h3>   
+                     <button onClick={this.onButtonClick}>Click Me!!</button> 
+            </div>    
+        );
+    }
+}
+
+
+
+
+
+ReactDOM.render(<ButtonClicks />, document.getElementById('root'));
